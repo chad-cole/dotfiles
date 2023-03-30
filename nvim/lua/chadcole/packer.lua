928d7837b71cdd6e8af718d7242f6a2aa348c99a
Chad Cole <chad.cole@shopify.com>
2023-03-30 11:58:11 -0400

    Updated colorscheme to melange

--- nvim/lua/chadcole/packer.lua
+++ nvim/lua/chadcole/packer.lua
@@ -21,13 +21,7 @@ return require('packer').startup(function(use)
         -- or                          , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-    use {
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    }
+    use { 'savq/melange-nvim' }
     use {
       'nvim-tree/nvim-tree.lua',
       requires = {
