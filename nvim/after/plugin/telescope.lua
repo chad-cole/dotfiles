25aecf441cf78907e7bc4ca1221eac1889a06312
Chad Cole <chad.cole@shopify.com>
2023-11-09 13:38:18 -0500

    Added convenience functions to telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,19 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>gs', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>fg', function()
+  local p = io.popen('git diff --name-only main...; git diff --name-only; git ls-files --others --exclude-standard')
+  local files = {}
+  for file in p:lines() do
+    table.insert(files, file)
+  end
+  p:close()
+
+  require('telescope.builtin').live_grep({ search_dirs = files })
+end, {})
