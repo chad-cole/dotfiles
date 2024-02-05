a0dee17c9254f6885da0f81a84e8924d58e5397f
Chad Cole <chad.cole@shopify.com>
2024-02-05 15:04:29 +0000

    Updated telescope plugin version

--- nvim/lua/chadcole/packer.lua
+++ nvim/lua/chadcole/packer.lua
@@ -17,8 +17,7 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                          , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
     use { 'savq/melange-nvim' }
