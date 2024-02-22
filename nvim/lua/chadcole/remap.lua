a8a5f92e36dad6768a97c52bda0095ed03c60fba
Chad Cole <chad.cole@shopify.com>
2024-02-22 20:17:29 +0000

    Remap quickfix navigation

--- nvim/lua/chadcole/remap.lua
+++ nvim/lua/chadcole/remap.lua
@@ -36,8 +36,8 @@ vim.keymap.set("n", "<M-m>", ":vertical resize -5<CR>", { silent = true })
 vim.keymap.set("n", "<leader>c", ':let @* = expand("%")<CR>', { silent = true })
 vim.keymap.set("n", "<leader>cc", ':let @+ = expand("%")<CR>', { silent = true })
 
-vim.keymap.set("n", "<Tab>", ":cn<CR>")
-vim.keymap.set("n", "<S-Tab>", ":cp<CR>")
+vim.keymap.set("n", "<M-o>", ":cn<CR>", { silent = true })
+vim.keymap.set("n", "<M-i>", ":cp<CR>", { silent = true })
 
 if(not os.getenv('$TMUX'))
 then
