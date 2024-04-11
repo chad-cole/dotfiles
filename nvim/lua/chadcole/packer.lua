3efba11d1635e6b8e23bfbe2acd0209c18783108
Chad Cole <chad.cole@shopify.com>
2024-04-11 13:59:31 +0000

    Added tree sitter context

--- nvim/lua/chadcole/packer.lua
+++ nvim/lua/chadcole/packer.lua
@@ -28,6 +28,7 @@ return require('packer').startup(function(use)
       },
     }
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter-context')
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
