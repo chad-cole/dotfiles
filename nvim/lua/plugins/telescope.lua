acd6b2a5c9c3aea4416e0881d187117e2ccec1f8
Chad Cole <chad.cole@shopify.com>
2024-09-06 13:04:10 -0400

    Tweaked telescope to be descending

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,8 @@
 return {
-  -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
+	-- change some telescope options and a keymap to browse plugin files
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
       -- add a keymap to browse plugin files
       -- stylua: ignore
       {
@@ -10,15 +10,15 @@ return {
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
         desc = "Find Plugin File",
       },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "bottom" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
+		},
+		-- change some options
+		opts = {
+			defaults = {
+				layout_strategy = "horizontal",
+				layout_config = { prompt_position = "bottom" },
+				sorting_strategy = "descending",
+				winblend = 0,
+			},
+		},
+	},
 }
