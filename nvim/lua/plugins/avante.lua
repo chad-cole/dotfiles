5264186ffef10a0e4bb8cf393ab031017e5f0145
Chad Cole <chad.cole@shopify.com>
2024-11-13 14:20:15 -0500

    Added Avante script from Prater'

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,52 +1,72 @@
+---@type LazySpec
 return {
-	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		lazy = false,
-		version = false, -- set this if you want to always pull the latest change
-		opts = {
-			provider = "openai",
-			openai = {
-				endpoint = os.getenv("OPENAI_API_CHAT_COMPLETIONS"),
-				model = "anthropic:claude-3-5-sonnet",
-				timeout = 30000, -- Timeout in milliseconds
-				temperature = 0,
-				max_tokens = 4096,
-				["local"] = false,
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	lazy = false,
+	version = false,
+	config = function()
+		local openai = require("avante.providers.openai")
+		local has_openai_key = vim.fn.executable("/Users/chadcole/dotfiles/openai_key") == 1
+
+		require("avante").setup({
+			-- @type AvanteProvider
+			provider = has_openai_key and "shopify-ai" or nil,
+			vendors = {
+				["shopify-ai"] = has_openai_key and {
+					endpoint = "https://proxy.shopify.ai/v3/v1",
+					model = "anthropic:claude-3-5-sonnet",
+					api_key_name = "cmd:/Users/chadcole/dotfiles/openai_key cat",
+					parse_curl_args = openai.parse_curl_args,
+					parse_response_data = openai.parse_response,
+				} or nil,
+			},
+			highlights = {
+				diff = {
+					incoming = "DiffChange",
+				},
+			},
+			mappings = {
+				suggestion = {
+					next = "]]",
+					prev = "[[",
+				},
+			},
+		})
+	end,
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	build = "make",
+	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	dependencies = {
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		--- The below dependencies are optional,
+		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		"zbirenbaum/copilot.lua", -- for providers='copilot'
+		{
+			-- support for image pasting
+			"HakonHarnes/img-clip.nvim",
+			event = "VeryLazy",
+			opts = {
+				-- recommended settings
+				default = {
+					embed_image_as_base64 = false,
+					prompt_for_file_name = false,
+					drag_and_drop = {
+						insert_mode = true,
+					},
+					-- required for Windows users
+					use_absolute_path = true,
+				},
 			},
 		},
-		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-		build = "make",
-		dependencies = {
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			{
-				-- support for image pasting
-				"HakonHarnes/img-clip.nvim",
-				event = "VeryLazy",
-				opts = {
-					-- recommended settings
-					default = {
-						embed_image_as_base64 = false,
-						prompt_for_file_name = false,
-						drag_and_drop = {
-							insert_mode = true,
-						},
-						-- required for Windows users
-						use_absolute_path = true,
-					},
-				},
+		{
+			-- Make sure to set this up properly if you have lazy=true
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {
+				file_types = { "markdown", "Avante" },
 			},
-			{
-				-- Make sure to set this up properly if you have lazy=true
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
+			ft = { "markdown", "Avante" },
 		},
 	},
 }
